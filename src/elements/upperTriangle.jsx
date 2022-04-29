ee5071cc8c1673f395499a6e5777a7b50d8babeb
Ivan Cheng <ivanchengzhongyan@gmail.com>
2022-04-29 20:07:31 +0800

    second commit

---
+++ src/elements/upperTriangle.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function UpperTriangle() {
+  return (
+    <svg className="logo3"
+      xmlns="http://www.w3.org/2000/svg"
+      width="93"
+      height="100"
+      fill="none"
+      viewBox="0 0 93 100"
+    >
+      <path
+        fill="url(#paint0_linear_3463_25008)"
+        d="M81.594 98.165L3.69 55.336a7.238 7.238 0 01-2.707-2.563A6.649 6.649 0 010 49.287a6.653 6.653 0 011.002-3.48 7.247 7.247 0 012.721-2.55L81.627.944A7.862 7.862 0 0185.362 0a7.864 7.864 0 013.739.933 7.252 7.252 0 012.739 2.56 6.656 6.656 0 011.003 3.498v85.142a6.655 6.655 0 01-1.007 3.508 7.254 7.254 0 01-2.75 2.562 7.866 7.866 0 01-3.752.924 7.858 7.858 0 01-3.74-.962z"
+        opacity="0.85"
+      ></path>
+      <path
+        fill="url(#paint1_linear_3463_25008)"
+        fillOpacity="0.9"
+        d="M81.594 98.165L3.69 55.336a7.238 7.238 0 01-2.707-2.563A6.649 6.649 0 010 49.287a6.653 6.653 0 011.002-3.48 7.247 7.247 0 012.721-2.55L81.627.944A7.862 7.862 0 0185.362 0a7.864 7.864 0 013.739.933 7.252 7.252 0 012.739 2.56 6.656 6.656 0 011.003 3.498v85.142a6.655 6.655 0 01-1.007 3.508 7.254 7.254 0 01-2.75 2.562 7.866 7.866 0 01-3.752.924 7.858 7.858 0 01-3.74-.962z"
+        opacity="0.85"
+      ></path>
+      <defs>
+        <linearGradient
+          id="paint0_linear_3463_25008"
+          x1="12.51"
+          x2="161.421"
+          y1="110.394"
+          y2="-13.337"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#fff"></stop>
+          <stop offset="1" stopColor="#fff"></stop>
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_3463_25008"
+          x1="12.51"
+          x2="161.421"
+          y1="110.394"
+          y2="-13.337"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#1A24E4"></stop>
+          <stop offset="1" stopColor="#F0F0F0" stopOpacity="0"></stop>
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
+
+export default UpperTriangle;
